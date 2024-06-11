46889314d72dbf6ebba41df6b22c5efc82489649
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-06-11 14:58:28 +0200

    with comments

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-let countEl = document.getElementById("count-el")
-let incrementBtn = document.getElementById("increment-btn")
+let countEl = document.getElementById("count-el") //keeps track of the count
+let incrementBtn = document.getElementById("increment-btn") //utilized to trigger the increase
 
 let count = 0
-countEl.innerText = count
+countEl.innerText = count //displays count in html
 
-incrementBtn.addEventListener("click",function(){
+incrementBtn.addEventListener("click",function(){ //modifies count on trigger
     count++
     console.log(count)
 })
