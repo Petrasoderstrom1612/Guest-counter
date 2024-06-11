05b6de2fb3f4a16578c74044656476767bc16c80
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-06-11 11:38:59 +0200

    my solution to incrementing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-document.getElementById("count-el").innerText = 5
+
+let countEl = document.getElementById("count-el")
+let incrementBtn = document.getElementById("increment-btn")
+
+let count = 0
+countEl.innerText = count
+
+incrementBtn.addEventListener("click",function(){
+    count++
+    console.log(count)
+})
